865908c2503958727c3b7f40325a5dfb48b30cc0
insiares <Insia.resistance@gmail.com>
2024-03-25 13:15:30 +0100

    linted

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import openai
 import discord
-import os
 import yaml
 import json
 
@@ -10,16 +9,20 @@ with open("cred.yml", "r") as stream:
     except yaml.YAMLError as exc:
         print(exc)
 
-token = cred['BOT_TOKEN']
-openai.api_key = cred['OPENAI_API_TOKEN']
+token = cred["BOT_TOKEN"]
+openai.api_key = cred["OPENAI_API_TOKEN"]
 
 
 # current_conv
 
-msg_system = '''Tu es LacDuSchultz, le bot Discord qui navigue sur l'océan infini des Internets avec la grâce d'un cygne et la précision d'un laser. Augmenté par des résultats de recherche
-contenus dans le message suivant, synthétise au mieux les resultats pour l'utilisateur.'''
+msg_system = """Tu es LacDuSchultz, le bot Discord qui navigue sur l'océan 
+infini des Internets avec la grâce d'un cygne et la précision d'un laser. 
+Augmenté par des résultats de recherche
+contenus dans le message suivant, synthétise au mieux les resultats 
+pour l'utilisateur."""
 
-current_conv = [{"role":"system", "content": msg_system}]
+current_conv = [{"role": "system", "content": msg_system}]
+
 
 def call_api(msg):
     with open("results.json", "r") as stream:
@@ -27,21 +30,21 @@ def call_api(msg):
             results = json.load(stream)
         except json.JSONDecodeError as exc:
             print(exc)
-    return f'{results}'
+    return f"{results}"
+
 
 # connect to discord
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+
 def chatgpt_reply(conv):
-
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", 
-        messages=conv,
-        max_tokens = 350
-        )
+        model="gpt-3.5-turbo", messages=conv, max_tokens=350
+    )
 
-    return completion['choices'][0]['message']['content']
+    return completion["choices"][0]["message"]["content"]
+
 
 # log
 @client.event
@@ -52,19 +55,22 @@ async def on_ready():
 # answerer
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
 
-    if message.channel.name == "général" and client.user.mentioned_in(message) and message.mention_everyone is False:
+    if (
+        message.channel.name == "général"
+        and client.user.mentioned_in(message)
+        and message.mention_everyone is False
+    ):
         async with message.channel.typing():
             msg = message.content
             msg = str(msg)
             resultat_api = call_api(msg)
-            current_conv.append({"role":"user", "content":msg})
-            current_conv.append({"role":"system", "content":resultat_api})
+            current_conv.append({"role": "user", "content": msg})
+            current_conv.append({"role": "system", "content": resultat_api})
             reply = chatgpt_reply(current_conv)
-            current_conv.append({"role":"assistant", "content":reply})
+            current_conv.append({"role": "assistant", "content": reply})
         await message.reply(reply, mention_author=True)
         # if the current_conv contains more than 10 messages, pop 2 messages
         if len(current_conv) > 10:
